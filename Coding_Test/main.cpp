2c68cdefaccabfe1bf2d9bbbb6bf3cd7cd22482f
SEUNGMIN_BAEK <seungmin03ws@gmail.com>
2026-06-13 16:01:12 +0900

    [Programmers] : (코딩 기초 트레이닝 Day1) "대소문자 바꿔서 출력하기"

--- Coding_Test/main.cpp
+++ Coding_Test/main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <string>
+
+#include <cctype>
 
 using namespace std;
 
-int main()
+int main(void)
 {
-	cout << "Hello Wolrd" << endl;
+    /* 내 풀이 */
+    string str;
+    cin >> str;
 
-	return 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = tolower(str[i]);
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = toupper(str[i]);
+        }
+        else
+            return 0;
+    }
+    cout << str;
+
+    /* 좋다고 생각한 풀이 */
+    string str;
+    cin >> str;
+
+    for (char c : str)
+    {
+        if (c >= 'A' && c <= 'Z')
+            c += 'a' - 'A';
+        else
+            c -= 'a' - 'A';
+
+        cout << c;
+    }
+
+    return 0;
 }
